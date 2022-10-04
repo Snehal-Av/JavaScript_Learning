919f8a79e037c236e0c61a6cdfdfb7988dbc331c
Snehal-Av <snehalavatade31@gmail.com>
2022-10-04 17:28:04 +0530

    Added new Js code

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Snehal Sopan Avatade");
+console.log("MY HOBBIES:")
+console.log("photography");
+console.log("tree Plantation");
+console.log("drawing");
+console.log("MY STRENGTH:");
+console.info("good learner");
+console.info("creative Mind");
+console.info("hard worker");
+console.log("MY WEAKNESS")
+console.warn("emotions");
+console.warn("I don't say no if anyone ask for help");
+console.warn("bad handwriting");
+console.log(30+50);
+console.log(49*479);
+
