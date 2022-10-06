3bec34887effe455025ac7e8da930e36d4699b0b
Snehal-Av <snehalavatade31@gmail.com>
2022-10-06 19:57:29 +0530

    Implementing swap in JS
    Implementing swap in JS

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,20 @@
+console.log("Before variable");
+var sweety="Sweety";
+var cutie="Cutie";
+console.log(sweety ,cutie);
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("After Swap");
+console.log(sweety,cutie);
+
+console.log("Before Swap");
+var num1="100";
+var num2="200";
+var num3="300";
+console.log(num1,num2,num3);
+var num4=num2;
+num2=num3;
+num3=num1;
+console.log(num4,num2,num3);
+
