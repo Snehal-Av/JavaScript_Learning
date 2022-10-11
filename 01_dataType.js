1863c964e646e55d5170e84611a7b426184044a0
Snehal-Av <snehalavatade31@gmail.com>
2022-10-11 20:36:34 +0530

    Implement Data type
    Implement Data type

---
+++ 01_dataType.js
@@ -0,0 +1,6 @@
+var name="Snehal";
+var age=26;
+var is_married=true;
+console.log("Name="+ name + " "+"dataType="+typeof(name));
+console.log("age="+ age+" "+"dataType="+typeof(age));
+console.log("is_married="+" "+"dataType="+typeof(is_married));
